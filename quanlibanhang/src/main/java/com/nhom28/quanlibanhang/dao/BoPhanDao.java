655d411a40552d5e96fb9db5bd00f59de473186c
THANHPHONG <THANHPHONG@THANHPHONG-PC>
2016-10-23 17:23:55 +0700

    update ten nguoi quan ly

--- quanlibanhang/src/main/java/com/nhom28/quanlibanhang/dao/BoPhanDao.java
+++ quanlibanhang/src/main/java/com/nhom28/quanlibanhang/dao/BoPhanDao.java
@@ -3,6 +3,7 @@ package com.nhom28.quanlibanhang.dao;
 import java.sql.SQLException;
 import java.util.List;
 
+import com.nhom28.quanlibanhang.dto.BoPhanDto;
 import com.nhom28.quanlibanhang.pojo.BoPhan;
 
 public interface BoPhanDao extends GenericDao<BoPhan> {
@@ -10,12 +11,14 @@ public interface BoPhanDao extends GenericDao<BoPhan> {
 	/**
 	 * @return ResultSet
 	 */
-	public List<BoPhan> getAll() throws SQLException;
+	public List<BoPhanDto> getAll() throws SQLException;
 	
 	public void delete(Integer id) throws SQLException;
 	
-	public void update(Integer id, String tenBoPhan, 
-			int idNguoiQuanLy, String ghiChu, byte conQuanLy)
-			throws SQLException;
+	public void updateBoPhan(Integer id, String tenBoPhan, 
+			String ghiChu, byte conQuanLy) throws SQLException;
+	
+	public void updateNhanVien(Integer idNguoiQuanLy, 
+			String tenNguoiQuanLy) throws SQLException;
 	
 }
